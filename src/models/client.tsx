6d7d93537a3f864d22108dd5db2eaef93976ba6e
stevenlegere <stevenmarklegere@gmail.com>
2023-08-03 12:07:42 +0100

    supabase details

--- src/models/client.tsx
+++ src/models/client.tsx
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
-const supabase = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
 export {};
