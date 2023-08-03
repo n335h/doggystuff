092c41ae09a4b127fe2801d729f25a880872d5b0
stevenlegere <stevenmarklegere@gmail.com>
2023-08-03 19:36:12 +0100

    Adding functionality for sign up

--- src/models/client.tsx
+++ src/models/client.tsx
@@ -5,10 +5,34 @@ const supabaseUrl: string | undefined = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey: string | undefined = process.env.REACT_APP_SUPABASE_KEY;
 
 // Check if supabaseUrl and supabaseKey are defined before creating the client
-let supabase: SupabaseClient | null = null;
+let supabaseClient: SupabaseClient | null = null;
 
 if (supabaseUrl && supabaseKey) {
-  supabase = createClient(supabaseUrl, supabaseKey);
+  supabaseClient = createClient(supabaseUrl, supabaseKey);
 }
 
-export default supabase;
+// Functionality for signing in a user
+
+export async function SignInUser(email: string, password: string): Promise<boolean> {
+    try { 
+        const { data, error } = await supabaseClient?.auth.signInWithPassword({
+            email: email,
+            password: password,
+        }) || {};
+        if (error) {
+            console.error('Sign-in error:', error);
+            return false;
+        }
+        else {
+            // Sign in was successful
+            console.log('Sign-in successful');
+            return true;
+        }
+    }
+    catch (error) {
+        // Handle any further errors that may occur
+        console.error('Sign-in error:', error);
+        return false;
+}
+}
+export default supabaseClient;
