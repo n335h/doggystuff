90db53536b989c41e96e26a981d72f1cfcf7fadd
stevenlegere <stevenmarklegere@gmail.com>
2023-08-04 13:43:08 +0100

    Working on sign up

--- src/pages/SignInSignUp/SignInSignUp.tsx
+++ src/pages/SignInSignUp/SignInSignUp.tsx
@@ -16,10 +16,25 @@ function SignInSignUp({ isSignedIn, setIsSignedIn, setFilter }: SignInSignUpProp
         password: '',
     });
 
+    // State variable to control the visibilty of the sign up form
+    const [showSignUp, setShowSignUp] = useState(false);
+    const [showSignIn, setShowSignIn] = useState(false);
+
+    function handleSignUpClick() {
+        // Event handler for the sign up button
+        setShowSignUp(true); // Set showSignUp to true to display the sign up form
+        setShowSignIn(false); // Set showSignIn to false to hide the sign in form
+    }
+
+    function handleSignInClick() {
+        // Event handler for the sign in button
+        setShowSignUp(false); // Set showSignIn to true to display the sign in form
+        setShowSignIn(true); // Set showSignUp to false to hide the sign up form
+    }
     return <>
         <div id="sign-up-in-button-container">
             <button id="sign-up-in-button" onClick={() => setFilter('sign-up')}>Sign Up</button>
-            </div>"
+            </div>
     </>
 }
 
