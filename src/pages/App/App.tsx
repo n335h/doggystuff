965fc4296bf13698f037820a023ee47aff4a8f9b
stevenlegere <stevenmarklegere@gmail.com>
2023-08-04 12:22:06 +0100

    Error handling

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,13 +1,24 @@
 
 import './App.css';
+import React, { useState } from 'react';
 import SignInSignUp from '../SignInSignUp/SignInSignUp';
 
 
 function App() {
+
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [filter, setFilter] = useState('');
+
+  const signInSignUpProps = {
+    isSignedIn: isSignedIn,
+    setIsSignedIn: setIsSignedIn,
+    setFilter: setFilter,
+  };
+
   return (
     <div className="App">
      
-     <SignInSignUp/>
+     <SignInSignUp {...signInSignUpProps}/>
 
  <h1>hello this is the app</h1>
 
--- src/pages/SignInSignUp/SignInSignUp.tsx
+++ src/pages/SignInSignUp/SignInSignUp.tsx
@@ -1,1 +1,26 @@
-export default function SignInSignUp() {}
+import { useState } from 'react';
+import SignUp from './Components/SignUp/SignUp';
+
+interface SignInSignUpProps {
+    isSignedIn: boolean;
+    setIsSignedIn: (value: boolean) => void;
+    setFilter: (value: string) => void;
+}
+
+function SignInSignUp({ isSignedIn, setIsSignedIn, setFilter }: SignInSignUpProps) {
+    // This state variable 'formData' is used to store data for the sign up form
+    const [formData, setFormData] = useState({
+        first_Name: '',
+        last_Name: '',
+        email: '',
+        password: '',
+    });
+
+    return <>
+        <div id="sign-up-in-button-container">
+            <button id="sign-up-in-button" onClick={() => setFilter('sign-up')}>Sign Up</button>
+            </div>"
+    </>
+}
+
+export default SignInSignUp;
